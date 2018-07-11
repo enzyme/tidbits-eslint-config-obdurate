409fdafcc517755f86a5c12bf05c2d5979d04c5a
Tristan Strathearn <tristan.strathearn@khemistry.com.au>
2018-07-12 09:54:30 +1000

    Updates rule set and removes additional rules outlined in README.md.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ module.exports = {
   'extends': 'airbnb',
   'rules': {
     'no-multiple-empty-lines': ['error', { 'max': 1, 'maxBOF': 0, 'maxEOF': 1 }],
+    'no-magic-numbers': ['error', { 'enforceConst': true }],
     'array-bracket-newline': ['error', { 'multiline': true, 'minItems': 2 }],
     'array-element-newline': ['error', { 'multiline': true, 'minItems': 2 }],
     'camelcase': ['error'],
@@ -22,8 +23,10 @@ module.exports = {
     'complexity': ['error', { 'max': 3 }],
     'max-depth': ['error', 1],
     'max-lines': ['error', {'max': 200, 'skipBlankLines': true, 'skipComments': true}],
-    'max-lines-per-function': ['error', {'max': 12, 'skipBlankLines': true, 'skipComments': true}],
+    'max-lines-per-function': ['error', {'max': 20, 'skipBlankLines': true, 'skipComments': true}],
     'max-statements-per-line': ['error', { 'max': 1 }],
-    'max-classes-per-file': ['error', 1]
+    'max-classes-per-file': ['error', 1],
+    'no-extra-parens': ['error', 'all', { ignoreJSX: 'multi-line', 'enforceForArrowConditionals': true, }],
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', ".jsx"] }]
   }
 };
